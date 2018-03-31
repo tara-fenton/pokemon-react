6bdc23db872c5ced7d54cd1265512f04fac86f1b
Tara Fenton <tarafenton@Taras-MBP.fios-router.home>
2018-03-31 17:17:08 -0400

    api call for characters

--- poke_app/src/CharacterList.js
+++ poke_app/src/CharacterList.js
@@ -1,11 +1,33 @@
 import React, {Component} from 'react';
 
 class CharacterList extends Component {
+
+  constructor(props) {
+    super()
+    this.state = {
+      list: []
+    }
+  }
+  //const API :
+  componentDidMount(){
+    fetch('https://pokeapi.co/api/v2/pokemon/?limit=151')
+    .then(response => response.json())
+    .then(data => {
+      let characters = data.results.map((character) => {
+        return (
+          <div key={character.name}>
+            {character.name}
+          </div>
+        )
+      })
+      this.setState({list:characters});
+    })
+  }
   render () {
     return (
       <div className='characters'>
         <h2 className="center">Pokemon</h2>
-        <div className='list'>
+        <div className='list'>{this.state.list}
         </div>
       </div>
     )
